0858f6b155ec176d6a8f17463ef76963ff431979
Jim Gustafson <jim@signal.org>
2020-04-29 11:04:48 -0700

    Reference signalapp/webrtc@4044c

--- config/version.sh
+++ config/version.sh
@@ -9,7 +9,7 @@
 
 # Specify WebRTC version.  This corresponds to the
 # branch or tag of the signalapp/webrtc repository.
-WEBRTC_VERSION="4044"
+WEBRTC_VERSION="4044c"
 
 RINGRTC_MAJOR_VERSION=1
 RINGRTC_MINOR_VERSION=3
