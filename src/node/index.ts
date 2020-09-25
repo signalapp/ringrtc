42d0b12c6805b1ad570bf24fb992274c7a5b4ce3
Jim Gustafson <jim@signal.org>
2020-09-25 14:09:38 -0700

    Expose more message types

--- src/node/index.ts
+++ src/node/index.ts
@@ -5,8 +5,29 @@
 // SPDX-License-Identifier: GPL-3.0-only
 //
 
-export { AudioDevice, Call, CallId, CallEndedReason, CallLogLevel, CallState, CallingMessage, CallSettings, DeviceId, RingRTCType, UserId, VideoCapturer, VideoRenderer } from './ringrtc/Service';
-export { CanvasVideoRenderer, GumVideoCapturer } from './ringrtc/VideoSupport';
+export {
+    AudioDevice,
+    Call,
+    CallId,
+    CallEndedReason,
+    CallLogLevel,
+    CallState,
+    CallingMessage,
+    CallSettings,
+    DeviceId,
+    HangupMessage,
+    HangupType,
+    RingRTCType,
+    OfferType,
+    UserId,
+    VideoCapturer,
+    VideoRenderer
+} from './ringrtc/Service';
+
+export {
+    CanvasVideoRenderer,
+    GumVideoCapturer
+} from './ringrtc/VideoSupport';
 
 import { RingRTCType } from './ringrtc/Service';
 export const RingRTC = new RingRTCType();
