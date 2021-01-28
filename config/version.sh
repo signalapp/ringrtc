2d30bda80de0a9ab350b1d94a8f420fd8f4da3d1
Jim Gustafson <jim@signal.org>
2021-01-28 15:27:00 -0800

    Add setOrientation() API to camera interface

--- config/version.sh
+++ config/version.sh
@@ -7,7 +7,7 @@
 
 # Specify WebRTC version.  This corresponds to the
 # branch or tag of the signalapp/webrtc repository.
-WEBRTC_VERSION="4183j"
+WEBRTC_VERSION="4183k"
 
 RINGRTC_MAJOR_VERSION=2
 RINGRTC_MINOR_VERSION=9
--- src/android/api/org/signal/ringrtc/CameraControl.java
+++ src/android/api/org/signal/ringrtc/CameraControl.java
@@ -6,6 +6,7 @@
 package org.signal.ringrtc;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import org.webrtc.CapturerObserver;
 
@@ -23,5 +24,20 @@ public interface CameraControl {
   public void setEnabled(boolean enable);
 
   public void flip();
+
+  /**
+   *
+   * Sets the orientation of the camera to a rotation defined by the
+   * application. The orientation (how the device is rotated) can be
+   * any of the integer degree values (i.e. 0, 90, 180, 270) defined
+   * by: CameraCharacteristics::SENSOR_ORIENTATION
+   *
+   * If not called or if null is set as the orientation, the default
+   * behavior will be used (CameraSession.getDeviceOrientation()).
+   *
+   * @param orientation  the current rotation value of the device
+   *
+   */
+  public void setOrientation(@Nullable Integer orientation);
 
 }
