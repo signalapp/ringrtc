3d8bc5e82901c48ae967269161e671f72f90a345
Peter Thatcher <peter@signal.org>
2022-01-28 11:55:26 -0700

    Update to WebRTC 4638f (3 spatial layers for VGA)

--- config/version.sh
+++ config/version.sh
@@ -10,7 +10,7 @@
 
 # Specify WebRTC version.  This corresponds to the
 # branch or tag of the signalapp/webrtc repository.
-WEBRTC_VERSION="4638e"
+WEBRTC_VERSION="4638f"
 
 RINGRTC_MAJOR_VERSION=2
 RINGRTC_MINOR_VERSION=17
