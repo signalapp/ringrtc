ae8b55d241bef3c7078515f53556fa950cac33cb
Rashad Sookram <rashad@signal.org>
2022-08-29 15:27:49 -0400

    Reference signalapp/webrtc@5005a
    This has new logs for when audio input starts.

--- config/version.sh
+++ config/version.sh
@@ -10,7 +10,7 @@
 
 # Specify WebRTC version.  This corresponds to the
 # branch or tag of the signalapp/webrtc repository.
-WEBRTC_VERSION=5005
+WEBRTC_VERSION=5005a
 
 RINGRTC_MAJOR_VERSION=2
 RINGRTC_MINOR_VERSION=20
