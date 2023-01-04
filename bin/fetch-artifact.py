7f312cfb6f0864f3cf6b6aef39e9f6237725757e
Jordan Rose <jrose@signal.org>
2023-01-04 12:42:23 -0800

    bin/fetch-artifact: fetch prebuilds from build-artifacts.signal.org
        # Fetch WebRTC prebuilds for a particular platform
        # (once we have recorded their checksums)
        bin/fetch-artifact -p android
        bin/fetch-artifact -p ios
        bin/fetch-artifact -p desktop   # infers current OS and arch
        bin/fetch-artifact -p mac-arm64 # but you can be specific
    
        # Fetch a different version's archive, if you have its checksum
        bin/fetch-artifact -p desktop --webrtc-version 12345-testing -c a1b2...
    
        # Fetch an arbitrary archive (and unpack it in OUTPUT_DIR)
        bin/fetch-artifact -u https://... -c a1b2...

---
+++ bin/fetch-artifact.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2023 Signal Messenger, LLC
+# SPDX-License-Identifier: AGPL-3.0-only
+#
+
+import argparse
+import hashlib
+import os
+import platform
+import sys
+import tarfile
+import urllib.request
+
+from typing import BinaryIO
+
+UNVERIFIED_DOWNLOAD_NAME = "unverified.tmp"
+
+PREBUILD_CHECKSUMS = {
+    'android': None,
+    'ios': None,
+    'windows-x64': None,
+    'windows-arm64': None,
+    'mac-x64': None,
+    'mac-arm64': None,
+    'linux-x64': None,
+    'linux-arm64': None,
+}
+
+
+def resolve_platform(platform_name: str) -> str:
+    if platform_name in PREBUILD_CHECKSUMS:
+        return platform_name
+
+    if platform_name in ['windows', 'mac', 'linux']:
+        arch_name = platform.machine().lower()
+        if arch_name in ['x86_64', 'amd64']:
+            return resolve_platform(platform_name + '-x64')
+        if arch_name in ['arm64', 'aarch64']:
+            return resolve_platform(platform_name + '-arm64')
+        raise AssertionError('unsupported architecture: ' + arch_name)
+
+    if platform_name == 'desktop':
+        os_name = platform.system().lower()
+        if os_name == 'darwin':
+            return resolve_platform('mac')
+        return resolve_platform(os_name)
+
+    raise AssertionError('unsupported platform: ' + platform_name)
+
+
+def build_argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description='Download and unpack a build artifact archive for a given platform, or from an arbitrary URL.')
+    source_group = parser.add_mutually_exclusive_group(required=True)
+    source_group.add_argument('-u', '--url',
+                              help='URL of an explicitly-specified artifact archive')
+    source_group.add_argument('-p', '--platform',
+                              help='WebRTC prebuild platform to fetch artifacts for')
+    parser.add_argument('-c', '--checksum',
+                        help='sha256sum of the unexpanded artifact archive (can be omitted for standard prebuilds)')
+
+    build_mode_group = parser.add_mutually_exclusive_group()
+    build_mode_group.add_argument('--debug', action='store_true',
+                                  help='Fetch debug prebuild instead of release')
+    build_mode_group.add_argument('--release', action='store_false', dest='debug',
+                                  help='Fetch release prebuild (default)')
+
+    parser.add_argument('--webrtc-version', metavar='TAG',
+                        help='WebRTC tag, used to identify a prebuild (provided implicitly if running through the shell wrapper)')
+    parser.add_argument('-o', '--output-dir',
+                        required=True,
+                        help='Build directory (provided implicitly if running through the shell wrapper)')
+    return parser
+
+
+def download_if_needed(archive_file: str, url: str, checksum: str) -> BinaryIO:
+    try:
+        f = open(archive_file, 'rb')
+        digest = hashlib.sha256()
+        chunk = f.read1()
+        while chunk:
+            digest.update(chunk)
+            chunk = f.read1()
+        if digest.hexdigest() == checksum.lower():
+            return f
+        print("existing file '{}' has non-matching checksum {}; re-downloading...".format(archive_file, digest.hexdigest()), file=sys.stderr)
+    except FileNotFoundError:
+        pass
+
+    print("downloading {}...".format(archive_file), file=sys.stderr)
+    try:
+        with urllib.request.urlopen(url) as response:
+            digest = hashlib.sha256()
+            f = open(UNVERIFIED_DOWNLOAD_NAME, 'w+b')
+            chunk = response.read1()
+            while chunk:
+                digest.update(chunk)
+                f.write(chunk)
+                chunk = response.read1()
+            assert digest.hexdigest() == checksum.lower(), "expected {}, actual {}".format(checksum.lower(), digest.hexdigest())
+            os.replace(UNVERIFIED_DOWNLOAD_NAME, archive_file)
+            return f
+    except urllib.error.HTTPError as e:
+        print(e, e.filename, file=sys.stderr)
+        sys.exit(1)
+
+
+def main() -> None:
+    parser = build_argument_parser()
+    args = parser.parse_args()
+    os.makedirs(os.path.abspath(args.output_dir), exist_ok=True)
+    os.chdir(args.output_dir)
+
+    url = args.url
+    checksum = args.checksum
+    if not url:
+        if not args.webrtc_version:
+            parser.error(message='--platform requires --webrtc-version')
+        platform = resolve_platform(args.platform)
+        build_mode = 'debug' if args.debug else 'release'
+        url = "https://build-artifacts.signal.org/libraries/webrtc-{}-{}-{}.tar.bz2".format(args.webrtc_version, platform, build_mode)
+        if not checksum:
+            checksum = PREBUILD_CHECKSUMS[platform]
+
+    if not checksum:
+        parser.error(message='missing --checksum')
+
+    archive_file = os.path.basename(url)
+    open_archive = download_if_needed(archive_file, url, checksum)
+
+    print("extracting {}...".format(archive_file), file=sys.stderr)
+    open_archive.seek(0)
+    tarfile.open(fileobj=open_archive).extractall()
+
+
+main()
