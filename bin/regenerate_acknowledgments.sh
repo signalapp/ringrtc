f6217f27bae4ee23fa8e230a9f8c0e35b095d091
Alex Konradi <akonradi@signal.org>
2023-12-13 14:00:59 -0500

    Use cargo-about 0.6.0 to produce acknowledgements
    The new version introduced a couple changes that are reflected here:
    
    - Rename override_git_commit -> override-git-commit in about.toml since
    that's now the canonical spelling.
    - Regenerate the license list since a bug was fixed that changed the
    semantics of the count.
    
    Also pin the version in our documentation and in the script to prevent
    differences in behavior depending on when cargo-about was installed on
    developer machines.

--- bin/regenerate_acknowledgments.sh
+++ bin/regenerate_acknowledgments.sh
@@ -10,6 +10,16 @@ set -euo pipefail
 SCRIPT_DIR=$(dirname "$0")
 cd "${SCRIPT_DIR}"/..
 
+echo "Checking cargo-about version"
+VERSION=$(cargo about --version)
+echo "Found $VERSION"
+
+EXPECTED_VERSION="cargo-about 0.6.0"
+if [ "$VERSION" != "$EXPECTED_VERSION" ]; then
+	echo "This tool works with $EXPECTED_VERSION but $VERSION is installed"
+	false
+fi
+
 for template in acknowledgments/*.hbs; do
     template_basename=$(basename "${template%.hbs}")
     echo "Generating ${template_basename}" ... >&2
