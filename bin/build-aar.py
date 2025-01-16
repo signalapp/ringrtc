cf0c6cf6061e64a62eff4bdc8c1f296c43459e79
Miriam Zimmerman <mutexlox@signal.org>
2025-01-16 11:33:29 -0500

    Update to webrtc 6834c

--- bin/build-aar.py
+++ bin/build-aar.py
@@ -215,7 +215,6 @@ def BuildArch(dry_run, project_dir, webrtc_src_dir, build_dir, arch, debug_build
             'rtc_enable_protobuf': 'false',
             'rtc_enable_sctp': 'false',
             'rtc_libvpx_build_vp9': 'false',
-            'rtc_include_ilbc': 'false',
             'rtc_disable_metrics': 'true',
             'rtc_disable_trace_events': 'true',
         }
